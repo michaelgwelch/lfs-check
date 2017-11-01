ecc2d9061a6d8ae2b7073d3681f270973e621ab9
Michael Welch <michaelgwelch@gmail.com>
2017-11-01 17:01:12 -0500

    Use `git log --numstat` to identify binaries
    Using the builtin git log command to identify binaries is
    much less code. It's more string processing, but I think
    it's easier to follow.
    
    Fixes #14

--- index.js
+++ index.js
@@ -1,197 +1,63 @@
 /* eslint-disable no-console */
 const cp = require('child_process');
 const util = require('util');
-const _ = require('lodash');
 
 const exec = util.promisify(cp.exec);
 
-// My new version will be based on this
-//  git log --numstat --no-renames  | awk '/^((-\t-\t)|(commit))/{print $0}'
-// But this shows renames as one delete line and one add line.
-// So I may want to run with renames first
-// git log --numstat  | awk '/^((-\t-\t)|(commit))/{print $0}'
-// and then check just those commits with renames.
-// else find the tool to convert path with rename syntax to final name
+function processCommitChunk(commitChunk) {
+  const lines = commitChunk.split('\n').filter(line => line.length !== '');
+  const commitObject = { id: undefined, binaries: [] };
 
-const EmptyTreeSha = '4b825dc642cb6eb9a060e54bf8d69288fbee4904';
+  if (lines.length > 1) {
+  // first line has the word commmit followed by the hash
+    const [, hash, ...words] = lines[0].split(/\s+/);
+    commitObject.id = hash;
+    commitObject.message = words.join();
 
+    const binaries = lines
+      .slice(1)
+      .filter(line => line.startsWith('-\t-\t'))
+      .map(line => line.split(/-\t-\t/))
+      .filter(splitResults => splitResults.length > 1)
+      .map(splitResults => splitResults[1]);
 
-async function normalizeCommitish(id) {
-  try {
-    const { stdout } = await exec(`git rev-parse --short ${id}`);
-    return stdout.trim();
-  } catch (e) {
-    throw new Error(`'${id}' is not a valid commit id`);
-  }
-}
-
-/**
- * Returns an object composed from key-value pairs. If a key is repeated then the value of that key
- * will be an array containing all of the values associated with that key.
- * <pre><code>
- * $ fromPairsRepeatingKeys([['one', 'hi'], ['two', 'bye'], ['one', 'hello']])
- * { one: [ 'hi', 'hello' ], two: 'bye' }
- * </code></pre>
- * @param {Array.<Array.<*>>} pairs - The key-value pairs
- */
-function fromPairsRepeatingKeys(pairs) {
-  const result = _.stubObject();
-
-  pairs.forEach((pair) => {
-    const [key, value] = pair;
-
-    if (_.has(result, key)) {
-      if (!_.isArray(result[key])) {
-        const originalValue = result[key];
-        result[key] = _.stubArray();
-        result[key].push(originalValue);
-      }
-      result[key].push(value);
-    } else {
-      result[key] = value;
-    }
-  });
-
-  return result;
-}
-
-/**
- * Represents a git commit
- * @property {string} tree - The sha of the tree of this commit
- * @property {Array.<string>} parents - The sha hashes of the parent commits
- * @property {string} id - The identifier of this commit (A sha hash, a branch name, HEAD, etc.)
- * @property {boolean} isMerge - Does this commit represent a merge commit?
- * (Equivalent to having 2 or more parents)
- * @property {boolean} isRoot - Is this the first commit in the repo?
- * (Equivalent to having 0 parents)
- */
-class Commit {
-  /**
-   * Creates a Commit instance
-   * @param {Object} o - A commit like object
-   * @param {string} o.tree - The sha of the tree of this commit
-   * @param {Array.<string>} o.parent - The sha hashes of the parent
-   * @param {string} o.id - The identifier of this commit (A sha hash, a branch name, HEAD, etc.)
-   */
-  constructor(o) {
-    const { parent, tree, id } = o;
-    this.parents = parent || [];
-    this.tree = tree;
-    this.id = id;
-    this.isMerge = parent.length > 1;
-    this.isRoot = parent.length === 0;
+    commitObject.binaries = binaries;
   }
 
-  static fromBuffer(buffer, id) {
-    const pairs = _
-      .chain(buffer.split('\n'))
-      .takeWhile(line => !line.startsWith('author'))
-      .map(line => line.split(/\s+/))
-      .value();
-
-    const commit = fromPairsRepeatingKeys(pairs);
-
-    commit.id = id;
-
-    // ensure `parent` is an array
-    // If it didn't exist in pairs, then parent won't exist, create it as an empty array.
-    // If it appeared once in pairs, then parent will be a scalar, convert to single element array
-    if (!_.isArray(commit.parent)) {
-      if (commit.parent) {
-        commit.parent = [commit.parent];
-      } else {
-        commit.parent = [];
-      }
-    }
-
-    return new Commit(commit);
-  }
+  return commitObject;
 }
 
-/**
- * Retrieves a commit object from the current git repository
- * @param {string} id - A commit identifier (sha, branch name, HEAD, etc.)
- * @returns {Promise<GitCommit>} Promise object that will return the commit.
- */
-async function getCommit(id) {
-  try {
-    const { stdout } = await exec(`git cat-file -p ${id}`);
-    return Commit.fromBuffer(stdout.trim(), id);
-  } catch (e) {
-    throw new Error(`'${id}' is not a valid commit id`);
-  }
-}
+async function gitLogNumStat(branch) {
+  // git log --numstat --diff-filter=d --no-renames --format=format:'%h' master..branch
+  //  --numstat Just show number of added and deleted lines in text files, but just two
+  //      hypens (-) for binary files. The hyphens are used to find binaries with the regex
+  //      /-\t-\t/ (that is two hyphens, each followed by a tab)
+  //  --no-renames Don't use rename syntax. Instead show an added line and a deleted line. This
+  //      avoids the problem of trying to parse the rename syntax in a string like the
+  //      following: 'releases/alderaan/{ => images}/authentication-metasys-10-hld.jpg
+  //  --diff-filter=d Don't show deleted files in the output. The --no-renames causes a rename to
+  //      be shown as two files. But it's not clear which was added and which was removed. So I
+  //      use this diff-filter to remove the deleted file leaving just the added file.
+  //  --format=format:'%h %f' For the commit only print out the abbreviated hash and subject
+  //      line( the files that changed are still printed as specified earlier)
+
+  const { stdout } = await exec(`git log --numstat --no-renames --diff-filter=d --format=format:'commit %h %f' master..${branch}`);
+
+  // Output will be something like the following with a trailing blank line for each commit
+  // commit 1ff8e4d
+  // 2       2       releases/alderaan/scheduler/images/schedule-service-components.png
+  // 182     3       releases/alderaan/scheduler/scheduler-database-design.md
+  // -       -       releases/alderaan/scheduler/schedule-service-databse-design.xlsx
+  //
 
 
-/**
- * Returns a list of files that changed as a result of the specified commit.
- * @param {Commit} commit - The hash code of the commit to inspect
- * @returns {Array.<string>} - An array of file names.
- */
-async function getChangedFilesForCommit(commit) {
-  if (commit.isMerge) {
-    const { stdout } = await exec(`git diff-tree --cc --name-only ${commit.id}`);
-    // Skip the first result which is just the commit identifier
-    return stdout.trim().split('\n').slice(1);
-  }
+  const commits = stdout
+    .trim()
+    .split(/\n\n/)
+    .filter(entry => entry !== '')
+    .map(processCommitChunk);
 
-  const parentTree = (commit.isRoot) ? EmptyTreeSha : `${commit.id}^`;
-
-  // Single parent commit
-  const { stdout } = await exec(`git diff --name-only ${parentTree} ${commit.id}`);
-  return stdout.trim().split('\n');
-}
-
-/**
- * Uses git detection powers to decide if the specified version of the file in the commit
- * specified by sha is binary.
- * @param {*} sha
- * @param {*} file
- */
-async function isBinary(sha, file) {
-  const { stdout } = await exec(`git diff-tree --numstat ${EmptyTreeSha} ${sha} -- ${file}`);
-
-  // This is a magic pattern in the output when file is binary. See notes.md
-  return stdout.includes('-\t-\t');
-}
-
-/**
- * Checks the changed files in the specified commit and returns an array of
- * binary files that are checked into the repository. The files are identified in the
- * format 'commit:path'.
- * @param {Commit|string} [commit = 'HEAD'] commit - A commit identifier
- * @returns {Array.<string>} A list of binary files. Each file is identified by commit
- * and path in the format commit:path (eg. d7abc6:bin/image.png)
- */
-async function checkCommit(commit = 'HEAD') {
-  const actualCommit = (_.isString(commit)) ? await getCommit(commit) : commit;
-  const files = await getChangedFilesForCommit(actualCommit);
-
-  const results = await Promise.all(files.map(file => isBinary(actualCommit.id, file)));
-
-  return _
-    .chain(_.zip(files, results))
-    .filter(pair => pair[1])
-    .map(pair => `${actualCommit.id}:${pair[0]}`)
-    .value();
-}
-
-async function getCommitsAheadOfMaster(commit) {
-  try {
-    const { stdout } = await exec(`git log --format=oneline master..${commit}`);
-    return stdout.trim(0)
-      .split('\n')
-      .filter(line => line.length !== 0)
-      .map((commitLine) => {
-        const spaceIndex = commitLine.indexOf(' ');
-        const id = commitLine.slice(0, spaceIndex);
-        const message = commitLine.slice(spaceIndex).trim();
-        return { id, message };
-      });
-  } catch (e) {
-    console.log(`Unknown commit or branch identifer '${commit}`);
-    return [];
-  }
+  return commits;
 }
 
 async function getCurrentBranch() {
@@ -227,6 +93,6 @@ async function getCurrentBranch() {
 // }
 
 module.exports = {
-  checkCommit, normalizeCommitish, getCommitsAheadOfMaster, getCurrentBranch,
+  gitLogNumStat, getCurrentBranch,
 };
 
