ef96d55fa6d1428697ab6fd767a950979853f9f4
Michael Welch <michaelgwelch@gmail.com>
2017-11-05 07:25:20 -0600

    Add test to make sure git returns what I expect.
    I've had a couple of regressions recently due to subtle
    changes in what was returned by git, when I modified
    switches. These regressions were unnoticed by me until
    after checked in. This integration test with git
    tests against two well known points in the repo and allows
    me to confirm I haven't broken something.

--- test/lfs-check.js
+++ test/lfs-check.js
@@ -1,34 +1,36 @@
 /* eslint-disable func-names, prefer-arrow-callback */
-const { parseGitBranch } = require('../lib/git-parsing');
+const { gitLogNumStat } = require('../lib');
+const _ = require('lodash');
 const assert = require('assert');
 
-describe('lfs-check', function () {
-  describe('parseGitBranch', function () {
-    it('returns the branch marked with an * ', function () {
-      const output = '* master\n';
-      const expected = 'master';
-
-      const actual = parseGitBranch(output);
-
-      assert.equal(actual, expected);
-    });
-
-    it('returns the branch marked with an * even if it\'s not the first one', function () {
-      const output = '  master\n' +
-                     '* feature\n';
-      const expected = 'feature';
-      const actual = parseGitBranch(output);
-
-      assert.equal(actual, expected);
-    });
-  });
-});
 
 describe('gitLogNumStat', function () {
-  it('expects a blank line between each commit returned by git', function () {
+  it('expects a blank line between each commit returned by git. ' +
+  'In addition any binary files should have numstat of -\\t-\\t. ' +
+  'Any binary files deleted in a commit should not show up in that commit', async function () {
     // Call gitLogNumStat with two well known tags in this repo that can
     // be used for integration testing.
+    const commits = await gitLogNumStat('compareForTests', 'baseForTests');
+    const expected = [
+      {
+        id: 'd4b9b2b',
+        binaries: [],
+        message: 'Add readme about testing integration with git',
+      },
+      {
+        id: '32bc0d5',
+        binaries: [],
+        message: 'Removing binary file',
+      },
+      {
+        id: 'e7383af',
+        binaries: [
+          'build-passing.png',
+        ],
+        message: 'Add binary file',
+      },
+    ];
 
-    
-  })
-}
+    assert(_.isEqual(commits, expected));
+  });
+});
